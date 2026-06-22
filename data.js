db282d72d663cd2d211f1b1a3db4af3ab2e1cc03
Ehwa Jang <46746950+2hwa-jang@users.noreply.github.com>
2026-06-22 20:55:08 +0900

    Update wedding data structure with additional details

--- data.js
+++ data.js
@@ -2,8 +2,15 @@ const weddingData = {
   groomName: "민석",
   brideName: "이화",
 
+  groomFullName: "민석",
+  brideFullName: "이화",
+
+  groomParents: "○○○ · ○○○",
+  brideParents: "○○○ · ○○○",
+
+  weddingDateISO: "2027-03-13T15:00:00+09:00",
   weddingDateText: "2027.03.13 SAT 15:00",
-  weddingDate: "2026년 3월 13일 토요일 오후 3시",
+  weddingTimeText: "2027년 3월 13일 토요일 오후 3시",
 
   invitationMessage: `
     서로의 가장 좋은 친구가 되어<br />
@@ -11,14 +18,87 @@ const weddingData = {
     소중한 걸음으로 축복해 주세요.
   `,
 
-  venueName: "웨딩시그니처 합정",
+  venueName: "웨딩 시그니처",
+  venueHall: "○○○홀",
   venueAddress: "서울 마포구 양화로 87",
 
-  kakaoMapLink: "https://place.map.kakao.com/803348028",
-  naverMapLink: "https://naver.me/xFLuQhhM",
+  tmapLink: "https://www.tmap.co.kr/",
+  kakaoMapLink: "https://map.kakao.com/",
+  naverMapLink: "https://map.naver.com/",
 
-  groomAccount: "은행명 000-0000-0000 예금주",
-  brideAccount: "은행명 000-0000-0000 예금주",
+  subwayInfo: "2호선, 6호선 합정역 하차 · 2번 출구 도보 4분",
+  busInfo: "서교동/우리은행서교동지점/합정역 하차",
+  parkingInfo: "내방하시는 차량은 건물 정문에서 주차 안내를 받으시기 바랍니다. 당일 모든 주차는 주차요원의 안내를 받아주세요.",
 
-  rsvpLink: "#"
+  galleryImages: [
+    "./images/gallery-01.jpg",
+    "./images/gallery-02.jpg",
+    "./images/gallery-03.jpg",
+    "./images/gallery-04.jpg",
+    "./images/gallery-05.jpg",
+    "./images/gallery-06.jpg",
+    "./images/gallery-07.jpg",
+    "./images/gallery-08.jpg",
+    "./images/gallery-09.jpg",
+    "./images/gallery-10.jpg",
+    "./images/gallery-11.jpg",
+    "./images/gallery-12.jpg",
+    "./images/gallery-13.jpg",
+    "./images/gallery-14.jpg",
+    "./images/gallery-15.jpg",
+    "./images/gallery-16.jpg",
+    "./images/gallery-17.jpg",
+    "./images/gallery-18.jpg",
+    "./images/gallery-19.jpg",
+    "./images/gallery-20.jpg",
+    "./images/gallery-21.jpg",
+    "./images/gallery-22.jpg",
+    "./images/gallery-23.jpg",
+    "./images/gallery-24.jpg",
+    "./images/gallery-25.jpg",
+    "./images/gallery-26.jpg",
+    "./images/gallery-27.jpg"
+  ],
+
+  groomAccounts: [
+    {
+      label: "신랑",
+      bank: "○○은행",
+      number: "000-0000-0000",
+      holder: "고민석"
+    },
+    {
+      label: "신랑 아버지",
+      bank: "○○은행",
+      number: "000-0000-0000",
+      holder: "○○○"
+    },
+    {
+      label: "신랑 어머니",
+      bank: "○○은행",
+      number: "000-0000-0000",
+      holder: "○○○"
+    }
+  ],
+
+  brideAccounts: [
+    {
+      label: "신부",
+      bank: "○○은행",
+      number: "000-0000-0000",
+      holder: "장이화"
+    },
+    {
+      label: "신부 아버지",
+      bank: "○○은행",
+      number: "000-0000-0000",
+      holder: "○○○"
+    },
+    {
+      label: "신부 어머니",
+      bank: "○○은행",
+      number: "000-0000-0000",
+      holder: "○○○"
+    }
+  ]
 };
