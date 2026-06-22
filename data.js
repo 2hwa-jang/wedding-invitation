164f8f3c4134022537b1a30929fd5e3fcf362720
Ehwa Jang <46746950+2hwa-jang@users.noreply.github.com>
2026-06-22 19:37:30 +0900

    Add wedding data object with details and links

---
+++ data.js
@@ -0,0 +1,24 @@
+const weddingData = {
+  groomName: "신랑",
+  brideName: "신부",
+
+  weddingDateText: "2026.00.00 SAT 00:00",
+  weddingDate: "2026년 00월 00일 토요일 오후 00시",
+
+  invitationMessage: `
+    서로의 가장 좋은 친구가 되어<br />
+    평생을 함께 걸어가려 합니다.<br />
+    소중한 걸음으로 축복해 주세요.
+  `,
+
+  venueName: "예식장명",
+  venueAddress: "서울특별시 ○○구 ○○로 00",
+
+  kakaoMapLink: "https://map.kakao.com/",
+  naverMapLink: "https://map.naver.com/",
+
+  groomAccount: "은행명 000-0000-0000 예금주",
+  brideAccount: "은행명 000-0000-0000 예금주",
+
+  rsvpLink: "#"
+};
